e7231bbab6b7f22b5f45626381025ac36b8706ef
Eman Nadeem <eman@codecapital.tech>
2023-12-26 11:38:11 +0500

    dev

---
+++ app/Http/Resources/FabricResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class FabricResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' =>$this->id,
+            'uuid' => $this->uuid,
+            'name' => $this->name,
+            'price' => $this->price,
+            'image'=> 'images/fabrics/',$this->image,
+            'composition'=>$this->composition,
+            'wight' => $this->weight,
+            'season' => $this->season,
+            'woven_by'=>$this->woven_by,
+            'fabric_code' => $this->fabric_code,
+
+        ]
+    }
+}
